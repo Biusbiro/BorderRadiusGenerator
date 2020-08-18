e5614c3d330299e98551fff09332cbc147706c21
Bismarck Renan <biusbiro@gmail.com>
2020-08-18 20:38:06 -0300

    Added Functions to change borders of example

--- javascript.js
+++ javascript.js
@@ -1,28 +1,48 @@
 function masterChange() {
     var value = document.getElementById("master").value;
-    document.getElementById("demo").innerHTML = value;
-    console.log(value)
+    document.getElementById("").innerHTML = value;
 }
 
 function topLeftChange() {
     var value = document.getElementById("top-left").value;
+    var rectangle = document.getElementById("rectangle");
+    changeBorderRadiusTopLeft(rectangle, value);
     document.getElementById("topLeft").innerHTML = value;
-    console.log(value)
 }
 
 function topRightChange() {
     var value = document.getElementById("top-right").value;
+    var rectangle = document.getElementById("rectangle");
+    changeBorderRadiusTopRight(rectangle, value);
     document.getElementById("topRight").innerHTML = value;
-    console.log(value)
 }
 
 function bottomLeftChange() {
     var value = document.getElementById("bottom-left").value;
+    var rectangle = document.getElementById("rectangle");
+    changeBorderRadiusBottomLeft(rectangle, value);
     document.getElementById("bottomLeft").innerHTML = value;
-    console.log(value)
 }
 
 function bottomRightChange() {
     var value = document.getElementById("bottom-right").value;
+    var rectangle = document.getElementById("rectangle");
+    changeBorderRadiusBottomRight(rectangle, value);
     document.getElementById("bottomRight").innerHTML = value;
+}
+
+function changeBorderRadiusBottomLeft(rectangle, value) {
+    rectangle.style.borderBottomLeftRadius = value + "px";
+}
+
+function changeBorderRadiusBottomRight(rectangle, value) {
+    rectangle.style.borderBottomRightRadius = value + "px";
+}
+
+function changeBorderRadiusTopLeft(rectangle, value) {
+    rectangle.style.borderTopLeftRadius = value + "px";
+}
+
+function changeBorderRadiusTopRight(rectangle, value) {
+    rectangle.style.borderTopRightRadius = value + "px";
 }
